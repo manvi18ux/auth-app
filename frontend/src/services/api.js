1113521ce9643da698abd1f8a86a60b75c306d89
Manvi Sinha <sinhamanvi539@gmail.com>
2026-02-09 13:36:38 +0530

    Connected frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //Base URL for the backend API
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/auth';
+const API_URL = process.env.REACT_APP_API_URL || 'https://auth-app-backend-wfnz.onrender.com';
 //Create axios instance
 const api = axios.create({
     baseURL: API_URL,
